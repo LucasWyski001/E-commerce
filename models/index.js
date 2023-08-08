c2d1235eeaa72196ac6cb3793833c568c7e84563
Lucas Wysoczanski <109931537+LucasWyski001@users.noreply.github.com>
2023-08-08 18:07:34 -0500

    added product routes

--- models/index.js
+++ models/index.js
@@ -3,29 +3,39 @@ const Product = require('./Product');
 const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
+
 // Products belongsTo Category
 Product.belongsTo(Category, {
-  foreignKey: 'product_id',
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE'
 });
+
 // Categories have many Products
+
 Category.hasMany(Product, {
-  foreignKey: 'product_id',
-  onDelete: 'CASCADE',
+  foreignKey: 'category_id',
 });
+
 // Products belongToMany Tags (through ProductTag)
+
 Product.belongsToMany(Tag, {
   through: {
     model: ProductTag,
     unique: false
-  }
+  },
+  as: 'productTag_products'
 });
+
 // Tags belongToMany Products (through ProductTag)
+
 Tag.belongsToMany(Product, {
   through: {
     model: ProductTag,
-    unique: false
-  }
+    unique: false,
+  },
+  as: 'productTag_products'
 });
+
 module.exports = {
   Product,
   Category,
--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -4,7 +4,7 @@ const { Category, Product } = require('../../models');
 router.get('/', async (req, res) => {
   try{
     const categoryData = await Category.findAll({
-      include: [{model: Product, through: product_id}]
+      include: [{model: Product}]
     });
     res.status(200).json(categoryData);
   }
@@ -14,15 +14,17 @@ router.get('/', async (req, res) => {
   // find all categories
   // be sure to include its associated Products
 });
+
 router.get('/:id', async (req, res) => {
   try{
     const categoryData = await Category.findByPk(req.params.id,{
-      include: [{ model: Product, through: product_id}]
+      include: [{ model: Product}]
     });
     if (!categoryData) {
       res.status(404).json({ message: 'No category found with this id!' });
       return;
     }
+    res.status(200).json(categoryData);
   }
   catch(err){
     res.status(500).json(err);
@@ -30,6 +32,7 @@ router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
 });
+
 router.post('/', async (req, res) => {
   try{
     const categoryData = await Category.create();
@@ -39,6 +42,7 @@ router.post('/', async (req, res) => {
     res.status(400).json(err);
   }
 });
+
 router.put('/:id', async (req, res) => {
   // update a category by its `id` value
   try{
@@ -49,6 +53,7 @@ router.put('/:id', async (req, res) => {
     res.status(400).json(err);
   }
 });
+
 router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
   try{
